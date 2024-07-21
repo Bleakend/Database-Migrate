a178b973ce5ac445ea017ccd12bc88d23162fc45
bleakend <bedelbi1221@gmail.com>
2024-07-22 00:42:51 +0300

    added config file to read parameters worked around with the sql server connections/drivers

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Initial-Commit
 # What's the tool about
 the aim of this tool is to **migrate** Excel files into a complete
 **database** with all the correct datatypes and relationship and so on
+
+# Packages Used
+pandas
+jinja2
+pyodbc
+pyyaml
+
+# Built-in packages used
+os
+sys
+logging
--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 import os
+import sys
 import logging
 import pandas as pd
-
+import pyodbc
+import yaml
 from Column import Column
 
-#Constants variables
-DIRECTORYPATH = r"Input Files/"
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s: %(message)s",
-    datefmt='%Y-%m-%d,%H:%M:%S',
-)
-
 #Helper functions
-def is_text_file(file_name: str) -> bool:
-    file_path = DIRECTORYPATH + file_name
+def parse_config_yaml():
+    try:
+        file = open(YAML_CONFIG_FILE, "r")
+        yaml_config = yaml.safe_load(file)
+    except Exception as e:
+        logging.error(f"Error faced when trying to read Yaml file: {e}")
+        sys.exit()
+
+    for item in yaml_config.values():
+        if(len(item.strip()) == 0):
+            logging.error("One or more config parameters are Empty!! Please check the config file.")
+            sys.exit()
+
+    server_name = yaml_config["Server name"]
+    database_name = yaml_config["Database name"]
+    input_directory = yaml_config["Input directory"]
+
+    return server_name, database_name, input_directory
+        
+
+def is_valid_extention(file_name: str) -> bool:
+    file_path = DIRECTORY_PATH + file_name
 
     if(not os.path.isfile(file_path)):
         return False
@@ -34,13 +47,32 @@ def is_empty_directory(input_files: list) -> bool:
     logging.warning("the directory has no files with the extention .txt! Please make sure The files are in the correct directory")
     return True
 
-#Main starting point
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s: %(message)s",
+    datefmt='%Y-%m-%d,%H:%M:%S',
+)
+
+#CONSTANTS
+YAML_CONFIG_FILE = r"config.yaml"
+SERVER_NAME, DATABASE_NAME, DIRECTORY_PATH = parse_config_yaml()
+
+
+connection = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
+                            f"Server={SERVER_NAME};"
+                            f"Database={DATABASE_NAME};"
+                            "Trusted_Connection=yes;")
+
+# cursor = connection.cursor()
+# cursor.execute("drop table test")
+# connection.commit()
+# connection.close()
+## Main starting point
 # input_files = os.listdir(DIRECTORYPATH)
-# input_files = list(filter(is_text_file, input_files))
+# input_files = list(filter(is_valid_extention, input_files))
 # is_empty_directory(input_files)
+
 
 d = {'a': 1, 'b': 2, 'c': 3.0}
 series = pd.Series(data=d, index=['a', 'b', 'c'], name="test")
 column = Column(series)
-
-print(column.column_type)
